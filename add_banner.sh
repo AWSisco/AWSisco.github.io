2f3d9049e7f235a3917bbd61bffe9a807ef6ed29
Adam.Adams-Surface <asisco@albany.edu>
2019-07-14 09:22:01 -0500

    Adding CV

--- add_banner.sh
+++ add_banner.sh
@@ -1,1 +1,1 @@
-sed -i '/<div class=\"container-fluid main-container\">/i<div class=\"banner-image\">\n<div class=\"attrib\">NASA Earth Observatory</div>\n</div>\n' index.html about.html blog.html projects.html cv.html contact.html
+sed -i '/<div class=\"container-fluid main-container\">/i<div class=\"banner-image\">\n<div class=\"attrib\">NASA Earth Observatory</div>\n</div>\n' index.html about.html blog.html projects.html contact.html
